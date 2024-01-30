749d7f814a2e293949b40a03f8b08a57abe036a3
Wild-er <wilder@algogems.io>
2024-01-30 16:16:33 -0500

    Modified vercel routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     let priceHistory = [];
   
     async function fetchData() {
-      const response = await fetch('/api/priceData');
+      const response = await fetch('/api');
       if (response.ok) {
         const data = await response.json();
         humbleLPInfo = data.humbleLPInfo;

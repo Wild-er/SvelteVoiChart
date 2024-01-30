777ab80b914a670ad2dd3a59b2759060e09cedf2
Wild-er <wilder@algogems.io>
2024-01-30 15:56:42 -0500

    Test backend v9

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     let priceHistory = [];
   
     async function fetchData() {
-      const response = await fetch('https://svelte-voi-chart.vercel.app/api');
+      const response = await fetch('https://svelte-voi-chart.vercel.app/api/priceData');
       if (response.ok) {
         const data = await response.json();
         humbleLPInfo = data.humbleLPInfo;
